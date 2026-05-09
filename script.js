d8c53e77a493161c1dbd1bdc31167895014bfacf
dhanglersondev <dhanglerson.dev@outlook.com>
2026-05-08 22:40:29 -0300

    Criando função para excluir tarefas

--- script.js
+++ script.js
@@ -4,20 +4,31 @@ let buttonElement = document.querySelector('#app button');
 
 let tasks = [];
 
-function renderTasks(){
+function renderTasks() {
     listElement.innerHTML = '';
 
     tasks.map((todo) => {
         let liElement = document.createElement("li");
         let taskText = document.createTextNode(todo);
 
+        let linkElement = document.createElement("a");
+        linkElement.setAttribute("href", "#");
+
+        let linkText = document.createTextNode("Excluir");
+        linkElement.appendChild(linkText);
+
+        let position = tasks.indexOf(todo);
+
+        linkElement.setAttribute("onclick", `deleteTask(${position})`)
+
         listElement.appendChild(taskText);
+        liElement.appendChild(linkElement);
         listElement.appendChild(liElement);
     })
 }
 
-function addTasks(){
-    if(inputElement.value === ''){
+function addTasks() {
+    if (inputElement.value === '') {
         alert('Informe uma tarefa');
         return false;
     } else {
@@ -31,3 +42,8 @@ function addTasks(){
 }
 
 buttonElement.onclick = addTasks;
+
+function deleteTask(position) {
+    tasks.splice(position, 1);
+    renderTasks();
+}
